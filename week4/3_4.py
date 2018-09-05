d1b6a8451cda67e6ba4df00c583a2933250cdbc2
Thebreak35 <3gthebreak@gmail.com>
2018-09-05 21:38:16 +0700

    3.4

--- week4/3_4.py
+++ week4/3_4.py
@@ -11,12 +11,12 @@ for t in range(0,60):
 		line = []
 		line = i.split(' ')
 		if line[14] == '80': #line[13] is srcport
-			if line[16] != '-' and '158.108.' not in line[16]:
+			if line[16] != '-' and '158.108.' in line[16]:
 				if line[16] not in dummy:
 					dummy[line[16]] = 1
 				else :
 					dummy[line[16]] += 1
-o = open('3_3.csv', 'w')
+o = open('3_4.csv', 'w')
 o.write('id,value\n')
 for i in dummy:
 	text = str(str(i) + ',' + str(dummy[i]))
